cf9088f1f7b39ee8c4116bdb64fb4b3b97bb362b
zniffu <129950600+zniffu@users.noreply.github.com>
2024-12-16 21:43:04 +1000

    Updated following tutorial to end of part 1

--- scene/main/main_screen.gd
+++ scene/main/main_screen.gd
@@ -3,9 +3,9 @@ extends Node2D
 # https://coolors.co/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529
 # https://coolors.co/d8f3dc-b7e4c7-95d5b2-74c69d-52b788-40916c-2d6a4f-1b4332-081c15
 # https://coolors.co/f8b945-dc8a14-b9690b-854e19-a03401
-#Colour palettes generated and used in the tutorial
+# Colour palettes generated and used in the tutorial
 
-#Dictionary constant of specified colours
+#Dictionary constant of specified colours for sprites (white png transparency ascii symbols)
 const PALETTE: Dictionary = {
 	&"BACKGROUND": "#212529",
 	&"LIGHT_GREY": "#ADB5BD",
@@ -20,11 +20,12 @@ const PALETTE: Dictionary = {
 
 #Setting the size of the 2D grid and the steps between (which match the dimensions of sprites.)
 const START_X: int = 50
-const START_Y: int = 54
-const STEP_X: int = 26
-const STEP_Y: int = 34
+const START_Y: int = 54 #starting pixel coords of the grid, visually offsets from edge of window xy pixels
+const STEP_X: int = 24 #steps reflect pixel size of the sprites tutorial 26
+const STEP_Y: int = 36 #tutorial 34, maybe error since the 4 and 6 are swapped according to sprite size
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready() -> void: #-> void is a best practice thing for return types, would produce same result without doing so.
 	RenderingServer.set_default_clear_color(PALETTE["BACKGROUND"])
 	_create_pc()
@@ -44,15 +45,45 @@ func _get_coord_from_sprite(sprite: Sprite2D) -> Vector2i:
 
 func _create_pc() -> void:
 	# var pc: Sprite2D = preload("res://sprite/pc.tscn").instantiate()
+	
 	var pc: PackedScene = preload("res://sprite/pc.tscn")
 	var new_pc: Sprite2D
 	var new_position: Vector2i = Vector2i(0, 0)
+	new_pc = pc.instantiate()
+	new_pc.position = _get_position_from_coord(new_position)
+	new_pc.modulate = PALETTE['GREEN']
+	new_pc.add_to_group("PC_TAG")
 
-	for i: StringName in PALETTE.keys():
-		new_pc = pc.instantiate()
-		new_pc.position = _get_position_from_coord(new_position)
-		new_pc.modulate = PALETTE[i]
-		add_child(new_pc)
-		print(_get_coord_from_sprite(new_pc))
-		new_position.x +=1
-		new_position.y +=1
+	# var pc: PackedScene = preload("res://sprite/pc.tscn")
+	# var new_pc: Sprite2D
+	# var new_position: Vector2i = Vector2i(0, 0)
+
+	# for i: StringName in PALETTE.keys():
+	# 	new_pc = pc.instantiate()
+	# 	new_pc.position = _get_position_from_coord(new_position)
+	# 	new_pc.modulate = PALETTE[i]
+	# 	add_child(new_pc)
+	# 	print(_get_coord_from_sprite(new_pc))
+	# 	print(_get_position_from_coord(new_position))
+	# 	new_position.y +=1
+
+func _move_pc(direction: StringName) -> void:
+	var pc: Sprite2D = get_tree().get_first_node_in_group(PC_TAG)
+	var coord: Vector2i = _get_coord_from_sprite(pc)
+
+# 	match direction:
+# 		MOVE_LEFT:
+# 			coord += Vector2i.LEFT
+# 		MOVE_RIGHT:
+# 			coord += Vector2i.RIGHT
+# 		MOVE_UP:
+# 			coord += Vector2i.UP
+# 		MOVE_DOWN:
+# 			coord += Vector2i.DOWN
+
+# 	pc.position = _get_position_from_coord(coord)
+
+# func _unhandled_input(event: InputEvent):
+# 	for i: StringName in MOVE_INPUTS:
+# 		if event.is_action_pressed(i):
+# 			_move_pc(i)
